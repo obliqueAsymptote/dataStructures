c0442a414f99dc629084ed7d4cbbc536c127fa41
ri <userinterfacey@tutanota.com>
2025-07-12 18:54:20 +0000

    i finished the pop nethod, and tested it and it works just as intended: after each push, it informs on where the next available space is, NOT the index. Im gonna lastly work on a return stack method so that after the stack is full, it returns the full stack with the elements before concluding my stacks data structure : )

--- Stack.cpp
+++ Stack.cpp
@@ -3,11 +3,14 @@
 #include <variant>
 #include <list>
 #include <stdexcept>
+#include <optional>
 
 using std::unique_ptr,
 std::string,
 std::variant,
-std::cout;
+std::cout,
+std::cin,
+std::optional;
 
 class Stack{
     private:
@@ -20,17 +23,41 @@ class Stack{
 
         bool push(variant<string, int> element){
             if (this->stack.size() >= stackSize){
-                cout << "Attempting to push onto a full stack";
+                cout << "Attempting to push onto a full stack\n";
                 return false;
             }
             stack.push_back(element);
             nextSpace += 1;
-            if (nextSpace >= stackSize){
-                cout << "Stack is now full";
+            if (nextSpace > stackSize){
+                cout << "Stack is now full\n";
                 return true;
             }
-            cout << "Next available space at position " << nextSpace;
+            cout << "Next available space at position " << nextSpace << "\n";
             return true;
 
         }
+        optional<variant<string, int>> pop(){
+            nextSpace -= 1;
+            if (nextSpace <= 0){
+                cout << "Stack is already empty\n attempting to pop nonexistant item\n";
+                return std::nullopt;
+            }
+            variant<string, int> poppedItem = stack.back();
+            stack.pop_back();
+            return poppedItem;
+        }
 };
+
+int main(){
+    int stackSize;
+    cout << "Declare stack size:\n";
+    cin >> stackSize;
+    Stack testStack(stackSize);
+    for (int i = 0; i < stackSize; i++){
+        int stackItem;
+        cout << "Enter item to push: \n";
+        cin >> stackItem;
+        testStack.push(stackItem);
+    }
+    return 0;
+}
