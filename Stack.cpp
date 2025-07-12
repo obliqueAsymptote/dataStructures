9f2c67f468c15f254009ab61065035018ee18d7b
ri <userinterfacey@tutanota.com>
2025-07-12 18:21:32 +0000

    so originally i wanted to implement a vector for the stack, but a list is much better because A. a list is more suited since i don't need to index, only retrieve the 'top' of the stack aka the backmost item at the list, B. it broadens my skillset working with another c++ data structure, and it only allows items to appended at the front or back which allows no random access like a vector would. As well, even though the nextSpace 'pointer' has not much actual functionality, it serves to make the stack behave like a stack should: indicating the next available space

--- Stack.cpp
+++ Stack.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <memory>
 #include <variant>
-#include <vector>
+#include <list>
+#include <stdexcept>
 
 using std::unique_ptr,
 std::string,
@@ -10,16 +11,26 @@ std::cout;
 
 class Stack{
     private:
-        std::vector<variant<string, int>> stack;
+        std::list<variant<string, int>> stack;
         const int stackSize;
         int nextSpace;
         
     public:
-        Stack(const int& size) : stackSize(size), nextSpace(0) {
-            stack.reserve(size);
-        }
+        Stack(const int& size) : stackSize(size), nextSpace(1) {}
 
-        void push(variant<string, int> element){
+        bool push(variant<string, int> element){
+            if (this->stack.size() >= stackSize){
+                cout << "Attempting to push onto a full stack";
+                return false;
+            }
+            stack.push_back(element);
+            nextSpace += 1;
+            if (nextSpace >= stackSize){
+                cout << "Stack is now full";
+                return true;
+            }
+            cout << "Next available space at position " << nextSpace;
+            return true;
 
         }
 };
