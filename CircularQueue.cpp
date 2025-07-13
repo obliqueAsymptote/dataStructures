77563202087a5a28e6c207446837b449ee2e1977
ri <userinterfacey@tutanota.com>
2025-07-13 01:11:51 +0000

    why do i never save anything before commiting man

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -1,2 +1,34 @@
 #include <iostream>
-#include <list>
+#include <variant>
+#include <vector>
+
+using
+std::variant,
+std::cout,
+std::string,
+std::vector;
+
+class CircularQueue{
+    private:
+        int headPointer;
+        int rearPointer;
+        int currentSize;
+        int capacity;
+        vector<variant<string, int>> queue;
+
+    public:
+        CircularQueue(const int& userCapacity) : headPointer(-1), rearPointer(-1), capacity(userCapacity) {
+            queue.resize(capacity);
+        }
+        void enqueue(const variant<string, int>& element){
+            if ((rearPointer + 1) % capacity == headPointer){
+                cout << "Stack is full";
+                return;
+            }
+            rearPointer = (rearPointer + 1) % capacity;
+            queue[rearPointer] = element;
+            currentSize ++;
+
+        }
+
+};
